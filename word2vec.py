5a905df59a1949fd02e68f0d03f07c5d2ed3f276
dorothy <jihyun14.p@gmail.com>
2019-09-10 11:31:21 +0900

    re-edit

--- word2vec.py
+++ word2vec.py
@@ -1,55 +1,28 @@
 # -*- coding: utf-8 -*- 
 from gensim.models import Word2Vec
 from flask import Flask, request, jsonify, send_file
-from flask_cors import CORS             # 테스트를 위해 CORS를 처리한다. 
-from collections import OrderedDict     # 순서를 기억하는 사전형 라이브러기
-
-import os                               # 파일에 접근하기 위한 라이브러리
+from flask_cors import CORS             
+import os                               
 import json
 
-app = Flask(__name__, static_folder='outputs')                   # 플라스크 웹 서버 객체를 생성한다.
+app = Flask(__name__, static_folder='outputs')                 
 CORS(app)
-
-file_data = OrderedDict()               # OrderedDict 타입의 file_data 생성
 
 def init():
     global model
-    model = Word2Vec.load('neighbor_model')     # Word2Vec 모델 loading
+    model = Word2Vec.load('neighbor_model')   
 
-def process_feature(neighbor):
-    js = model.most_similar(neighbor)           # 유사 관계 단어 추출
-    file_data["children"] = [{"word": js}]
-
-    path = 'outputs/{0}.json'.format(neighbor)  # json 파일 생성 위치
-
-    with open(path, 'w') as f:
-        json.dump(file_data, f, indent=1)
-
-    
-@app.route("/estimator", methods=['GET', 'POST'])
+@app.route("/estimator", methods=['POST'])
 def estimator():
     content = request.json
-    process_feature(content['neighbor'])
-    result = {'result': True}
+    js = model.most_similar(content['neighbor'])
+    result = {'word': js}
     return jsonify(result)
 
 @app.route("/outputs", methods=['GET', 'POST'])
 def outputs():
     neighbor_id = request.args.get('neighbor')
     return app.send_static_file(neighbor_id + '.json')
-
-@app.route("/validate", methods=['GET', 'POST'])
-def validate():
-    neighbor_id = request.args.get('neighbor')
-    path = "outputs/{0}.json".format(neighbor_id)
-    result = {}
-
-    # 해당 json 파일 존재하는지 확인
-    if os.path.isfile(path):
-        result['result'] = True
-    else:
-        result['result'] = False
-    return jsonify(result)
 
 if __name__ == '__main__':
     init()
